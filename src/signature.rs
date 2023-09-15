2b15230e18b81910013f163bfe3a0db1bd655203
rvanasa <ryan.vandersmith@dfinity.org>
2023-09-15 14:22:10 -0600

    Optimize verify logic

--- src/signature.rs
+++ src/signature.rs
@@ -1,6 +1,9 @@
-use ic_eth::core::types::{Address, RecoveryMessage, Signature};
+use ic_eth::core::types::{RecoveryMessage, Signature};
 
 pub fn do_verify_signature(eth_address: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+    let eth_address_bytes: [u8; 20] = eth_address
+        .try_into()
+        .unwrap_or_else(|_| ic_cdk::trap("expected 20-byte address"));
     if signature.len() != 65 {
         ic_cdk::trap("expected 65-byte signature");
     }
@@ -9,9 +12,11 @@ pub fn do_verify_signature(eth_address: Vec<u8>, message: Vec<u8>, signature: Ve
         s: signature[32..64].into(),
         v: signature[64].into(),
     }
-    .verify(
-        RecoveryMessage::Data(message),
-        Address::from_slice(&eth_address),
-    )
+    .verify(RecoveryMessage::Data(message), eth_address_bytes)
     .is_ok()
+}
+
+#[test]
+fn test_verify_signature() {
+    // TODO
 }
