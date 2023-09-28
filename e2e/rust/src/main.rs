dd606320082915c953c1da75e5db5ee910fd1a7f
rvanasa <ryan.vandersmith@dfinity.org>
2023-09-28 15:02:15 -0600

    Show human-readable canister error message in Rust E2E test

--- e2e/rust/src/main.rs
+++ e2e/rust/src/main.rs
@@ -1,7 +1,7 @@
 use candid::candid_method;
 use ic_cdk_macros::update;
 
-use e2e::ic_eth;
+use e2e::declarations::ic_eth::{ic_eth, Result_, Source};
 
 fn main() {}
 
@@ -11,10 +11,20 @@ pub async fn test() {
     // TODO: call with cycles
     let result = ic_eth
         .request(
-            Source::Network(1 /* Ethereum */),
-            "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":null,\"id\":1}",
+            Source::Chain(1 /* Ethereum */),
+            "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":null,\"id\":1}"
+                .to_string(),
             1000,
         )
-        .await;
-    assert!(result == Ok("{\"jsonrpc\":\"2.0\",\"result\":\"0x247a3fa65\",\"id\":1}"))
+        .await
+        .unwrap()
+        .0;
+    match result {
+        Result_::Ok(response) => assert_eq!(
+            response,
+            "{\"jsonrpc\":\"2.0\",\"result\":\"0x247a3fa65\",\"id\":1}",
+        ),
+        Result_::Err(err) => ic_cdk::trap(&format!("{}", err)),
+    }
 }
+
