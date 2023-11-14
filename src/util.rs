febb72d1e9d00d697602fbb7a139f2d265cbcb56
rvanasa <ryan.vandersmith@dfinity.org>
2023-11-14 11:00:03 -0700

    Remove commented code

--- src/util.rs
+++ src/util.rs
@@ -10,12 +10,6 @@ pub fn to_principal(principal: &str) -> Principal {
 
 pub fn canonicalize_json(text: &[u8]) -> Option<Vec<u8>> {
     let json = serde_json::from_slice::<Value>(text).ok()?;
-    // match &mut json {
-    //     Value::Object(ref mut obj) => {
-    //         obj.insert("canonical".to_string(), Value::Bool(true));
-    //     }
-    //     _ => (),
-    // };
     serde_json::to_vec(&json).ok()
 }
 
