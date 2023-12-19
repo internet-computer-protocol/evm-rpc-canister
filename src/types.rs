8060996ddf11435da392482b69df8c13b4edcece
rvanasa <ryan.vandersmith@dfinity.org>
2023-12-19 12:37:45 -0700

    Add 'MultiRpcResult::map()' test cases

--- src/types.rs
+++ src/types.rs
@@ -559,3 +559,61 @@ pub mod candid_types {
         }
     }
 }
+
+#[test]
+fn test_multi_rpc_result_map() {
+    let err = RpcError::ProviderError(ProviderError::ProviderNotFound);
+    assert_eq!(
+        MultiRpcResult::Consistent(Ok(5)).map(|n| n + 1),
+        MultiRpcResult::Consistent(Ok(6))
+    );
+    assert_eq!(
+        MultiRpcResult::Consistent(Err(err.clone())).map(|()| unreachable!()),
+        MultiRpcResult::Consistent(Err(err.clone()))
+    );
+    assert_eq!(
+        MultiRpcResult::Inconsistent(vec![(
+            RpcService::EthMainnet(EthMainnetService::Ankr),
+            Ok(5)
+        )])
+        .map(|n| n + 1),
+        MultiRpcResult::Inconsistent(vec![(
+            RpcService::EthMainnet(EthMainnetService::Ankr),
+            Ok(6)
+        )])
+    );
+    assert_eq!(
+        MultiRpcResult::Inconsistent(vec![
+            (RpcService::EthMainnet(EthMainnetService::Ankr), Ok(5)),
+            (
+                RpcService::EthMainnet(EthMainnetService::Cloudflare),
+                Ok(10)
+            )
+        ])
+        .map(|n| n + 1),
+        MultiRpcResult::Inconsistent(vec![
+            (RpcService::EthMainnet(EthMainnetService::Ankr), Ok(6)),
+            (
+                RpcService::EthMainnet(EthMainnetService::Cloudflare),
+                Ok(11)
+            )
+        ])
+    );
+    assert_eq!(
+        MultiRpcResult::Inconsistent(vec![
+            (RpcService::EthMainnet(EthMainnetService::Ankr), Ok(5)),
+            (
+                RpcService::EthMainnet(EthMainnetService::PublicNode),
+                Err(err.clone())
+            )
+        ])
+        .map(|n| n + 1),
+        MultiRpcResult::Inconsistent(vec![
+            (RpcService::EthMainnet(EthMainnetService::Ankr), Ok(6)),
+            (
+                RpcService::EthMainnet(EthMainnetService::PublicNode),
+                Err(err)
+            )
+        ])
+    );
+}
