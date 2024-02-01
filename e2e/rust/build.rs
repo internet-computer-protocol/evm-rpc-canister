188045940d5294733f2dd94b712c168c5f53c14f
rvanasa <ryan.vandersmith@dfinity.org>
2024-01-31 19:50:12 -0700

    Use 'evm_rpc_staging_fiduciary' in Rust E2E

--- e2e/rust/build.rs
+++ e2e/rust/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut builder = Builder::new();
 
     builder.add({
-        let mut config = Config::new("evm_rpc");
+        let mut config = Config::new("evm_rpc_staging_fiduciary");
         config.binding.type_attributes =
             "#[derive(CandidType, Clone, Debug, Deserialize)]".to_string();
         config
