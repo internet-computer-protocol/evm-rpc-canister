78c02ab9eb489fc9cee964180754f802be917a28
rvanasa <ryan.vandersmith@dfinity.org>
2024-03-07 16:32:06 -0700

    Add comment

--- e2e/rust/build.rs
+++ e2e/rust/build.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut builder = Builder::new();
 
     builder.add({
+        // Uppercase canister name is a workaround for using `ic-cdk-bindgen` with `dfx` >= 0.18.0
         let mut config = Config::new("EVM_RPC_STAGING_FIDUCIARY");
         config.binding.type_attributes =
             "#[derive(CandidType, Clone, Debug, Deserialize)]".to_string();
