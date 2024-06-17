2c2baea25a9c04a56e4f51f63a425aa76d0952ce
rvanasa <ryan.vandersmith@dfinity.org>
2024-06-17 13:15:32 -0600

    Use latest block for Rust E2E

--- e2e/rust/src/main.rs
+++ e2e/rust/src/main.rs
@@ -69,7 +69,7 @@ pub async fn test() {
             (),
             GetTransactionCountArgs {
                 address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".to_string(),
-                block: BlockTag::Number(20113080.into()),
+                block: BlockTag::Latest,
             },
         ),
         10000000000,
