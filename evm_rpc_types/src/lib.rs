22a1717053971adb0847662a3172cd445a204bb5
gregorydemay <gregory.demay@dfinity.org>
2024-08-22 17:42:09 +0200

    243: nat 256

--- evm_rpc_types/src/lib.rs
+++ evm_rpc_types/src/lib.rs
@@ -1,3 +1,4 @@
+use candid::types::{Serializer, Type};
 use candid::{CandidType, Nat};
 use serde::Deserialize;
 
@@ -12,5 +13,41 @@ pub enum BlockTag {
     Safe,
     Earliest,
     Pending,
-    Number(Nat),
+    Number(Nat256),
+}
+
+/// A `Nat` that is guaranteed to fit in 256 bits.
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
+#[serde(try_from = "candid::Nat")]
+pub struct Nat256(Nat);
+
+impl AsRef<Nat> for Nat256 {
+    fn as_ref(&self) -> &Nat {
+        &self.0
+    }
+}
+
+impl CandidType for Nat256 {
+    fn _ty() -> Type {
+        Nat::_ty()
+    }
+
+    fn idl_serialize<S>(&self, serializer: S) -> Result<(), S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_nat(self.as_ref())
+    }
+}
+
+impl TryFrom<Nat> for Nat256 {
+    type Error = String;
+
+    fn try_from(value: Nat) -> Result<Self, Self::Error> {
+        if value.0.to_bytes_le().len() > 32 {
+            Err("Nat does not fit in a U256".to_string())
+        } else {
+            Ok(Nat256(value))
+        }
+    }
 }
