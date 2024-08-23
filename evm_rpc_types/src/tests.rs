fb2a8c46085be21ec1a30d0dbe79515e970a7ff4
gregorydemay <gregory.demay@dfinity.org>
2024-08-23 15:46:26 +0200

    243: simplify test

--- evm_rpc_types/src/tests.rs
+++ evm_rpc_types/src/tests.rs
@@ -32,9 +32,9 @@ mod nat256 {
         #[test]
         fn should_convert_to_bytes_and_back(u256 in arb_u256()) {
             let value = Nat256::try_from(Nat::from(u256)).unwrap();
-
             let bytes = value.clone().into_be_bytes();
-            let value_from_bytes = Nat256::try_from(Nat::from(BigUint::from_bytes_be(&bytes))).unwrap();
+
+            let value_from_bytes = Nat256::from_be_bytes(bytes);
 
             assert_eq!(value, value_from_bytes);
         }
