7a13e050b550a7dec809ad030ce56ab15e4e2c68
gregorydemay <gregory.demay@dfinity.org>
2024-08-23 15:39:17 +0200

    Revert "243: undo Candid changes in FeeHistory, since not yet used"
    This reverts commit d5f3993b

--- evm_rpc_types/src/response/mod.rs
+++ evm_rpc_types/src/response/mod.rs
@@ -4,14 +4,18 @@ use candid::CandidType;
 #[derive(Debug, Clone, PartialEq, CandidType)]
 pub struct FeeHistory {
     /// Lowest number block of the returned range.
+    #[serde(rename = "oldestBlock")]
     pub oldest_block: Nat256,
     /// An array of block base fees per gas.
     /// This includes the next block after the newest of the returned range,
     /// because this value can be derived from the newest block.
     /// Zeroes are returned for pre-EIP-1559 blocks.
+    #[serde(rename = "baseFeePerGas")]
     pub base_fee_per_gas: Vec<Nat256>,
     /// An array of block gas used ratios (gasUsed / gasLimit).
+    #[serde(rename = "gasUsedRatio")]
     pub gas_used_ratio: Vec<f64>,
     /// A two-dimensional array of effective priority fees per gas at the requested block percentiles.
+    #[serde(rename = "reward")]
     pub reward: Vec<Vec<Nat256>>,
 }
