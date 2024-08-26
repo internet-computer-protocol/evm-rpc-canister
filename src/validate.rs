a00876db57b714f5e0d4d13c8920a776ab1af65f
rvanasa <ryan.vandersmith@dfinity.org>
2024-08-26 16:59:16 -0600

    Update API key validation test cases

--- src/validate.rs
+++ src/validate.rs
@@ -89,7 +89,11 @@ mod test {
     #[test]
     pub fn test_validate_api_key() {
         assert_eq!(validate_api_key("abc"), Ok(()));
-        assert_eq!(validate_api_key(".."), Err("Invalid character in API key"));
+        assert_eq!(
+            validate_api_key("?a=b"),
+            Err("Invalid character in API key")
+        );
+        assert_eq!(validate_api_key("/"), Err("Invalid character in API key"));
         assert_eq!(
             validate_api_key("abc/def"),
             Err("Invalid character in API key")
@@ -99,11 +103,7 @@ mod test {
             Err("Invalid character in API key")
         );
         assert_eq!(
-            validate_api_key("abc/.."),
-            Err("Invalid character in API key")
-        );
-        assert_eq!(
-            validate_api_key("../.."),
+            validate_api_key("abc/:key"),
             Err("Invalid character in API key")
         );
     }
